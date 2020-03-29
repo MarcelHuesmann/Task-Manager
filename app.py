b3bffa7fafdd7fea351d4d2f2bfcc1a09588075b
MarcelHuesmann <marcel-privat@mail.ru>
2020-03-29 12:22:48 +0000

    changes

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
-
 app.config["MONGO_DBNAME"] = 'taskmanager'
 app.config["MONGO_URI"] = 'mongodb+srv://root:rOOtUser@cluster0-s8vqf.mongodb.net/test?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
+
 
 @app.route('/')
 @app.route('/get_tasks')
